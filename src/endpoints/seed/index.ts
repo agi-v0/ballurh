8fa24c1b6a8c82e803ef48ec56093f890171625e
agi-v0 <omaroubari16@gmail.com>
2025-07-27 21:08:12 +0300

    fix endpoint types

--- src/endpoints/seed/index.ts
+++ src/endpoints/seed/index.ts
@@ -41,20 +41,29 @@ export const seed = async ({
   payload.logger.info(`— Clearing collections and globals...`)
 
   // clear the database
-  await Promise.all(
-    globals.map((global) =>
-      payload.updateGlobal({
-        slug: global,
-        data: {
-          navItems: [],
-        },
-        depth: 0,
-        context: {
-          disableRevalidate: true,
-        },
-      }),
-    ),
-  )
+  await Promise.all([
+    payload.updateGlobal({
+      slug: 'header',
+      data: {
+        tabs: [],
+        cta: [],
+      },
+      depth: 0,
+      context: {
+        disableRevalidate: true,
+      },
+    }),
+    payload.updateGlobal({
+      slug: 'footer',
+      data: {
+        columns: [],
+      },
+      depth: 0,
+      context: {
+        disableRevalidate: true,
+      },
+    }),
+  ])
 
   await Promise.all(
     collections.map((collection) => payload.db.deleteMany({ collection, req, where: {} })),
@@ -280,64 +289,64 @@ export const seed = async ({
     }),
   ])
 
-  payload.logger.info(`— Seeding globals...`)
+  // payload.logger.info(`— Seeding globals...`)
 
-  await Promise.all([
-    payload.updateGlobal({
-      slug: 'header',
-      data: {
-        navItems: [
-          {
-            link: {
-              type: 'custom',
-              label: 'Posts',
-              url: '/posts',
-            },
-          },
-          {
-            link: {
-              type: 'reference',
-              label: 'Contact',
-              reference: {
-                relationTo: 'pages',
-                value: contactPage.id,
-              },
-            },
-          },
-        ],
-      },
-    }),
-    payload.updateGlobal({
-      slug: 'footer',
-      data: {
-        navItems: [
-          {
-            link: {
-              type: 'custom',
-              label: 'Admin',
-              url: '/admin',
-            },
-          },
-          {
-            link: {
-              type: 'custom',
-              label: 'Source Code',
-              newTab: true,
-              url: 'https://github.com/payloadcms/payload/tree/main/templates/website',
-            },
-          },
-          {
-            link: {
-              type: 'custom',
-              label: 'Payload',
-              newTab: true,
-              url: 'https://payloadcms.com/',
-            },
-          },
-        ],
-      },
-    }),
-  ])
+  // await Promise.all([
+  //   payload.updateGlobal({
+  //     slug: 'header',
+  //     data: {
+  //       navItems: [
+  //         {
+  //           link: {
+  //             type: 'custom',
+  //             label: 'Posts',
+  //             url: '/posts',
+  //           },
+  //         },
+  //         {
+  //           link: {
+  //             type: 'reference',
+  //             label: 'Contact',
+  //             reference: {
+  //               relationTo: 'pages',
+  //               value: contactPage.id,
+  //             },
+  //           },
+  //         },
+  //       ],
+  //     },
+  //   }),
+  //   payload.updateGlobal({
+  //     slug: 'footer',
+  //     data: {
+  //       navItems: [
+  //         {
+  //           link: {
+  //             type: 'custom',
+  //             label: 'Admin',
+  //             url: '/admin',
+  //           },
+  //         },
+  //         {
+  //           link: {
+  //             type: 'custom',
+  //             label: 'Source Code',
+  //             newTab: true,
+  //             url: 'https://github.com/payloadcms/payload/tree/main/templates/website',
+  //           },
+  //         },
+  //         {
+  //           link: {
+  //             type: 'custom',
+  //             label: 'Payload',
+  //             newTab: true,
+  //             url: 'https://payloadcms.com/',
+  //           },
+  //         },
+  //       ],
+  //     },
+  //   }),
+  // ])
 
   payload.logger.info('Seeded database successfully!')
 }
