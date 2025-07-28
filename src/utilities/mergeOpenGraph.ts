6c6045e2e450985f7f0fe78956fed0e12c9f5b8d
agi-v0 <omaroubari16@gmail.com>
2025-07-28 08:56:05 +0300

    set default open graph data

--- src/utilities/mergeOpenGraph.ts
+++ src/utilities/mergeOpenGraph.ts
@@ -3,14 +3,15 @@ import { getServerSideURL } from './getURL'
 
 const defaultOpenGraph: Metadata['openGraph'] = {
   type: 'website',
-  description: 'An open-source website built with Payload and Next.js.',
-  images: [
-    {
-      url: `${getServerSideURL()}/website-template-OG.webp`,
-    },
-  ],
-  siteName: 'Payload Website Template',
-  title: 'Payload Website Template',
+  description:
+    '‏زد مبيعاتك، تنبأ بسلوكيات الطلب وقم بتحسين مؤشرات الأداء من خلال إتخاذ قرارات نمو تعتمد على البيانات باستخدام قوة تقنيات ذكاء الأعمال.',
+  // images: [
+  //   {
+  //     url: `${getServerSideURL()}/website-template-OG.webp`,
+  //   },
+  // ],
+  siteName: 'بلّورة',
+  title: 'بلّورة | منصة ذكاء أعمال لقطاع التجزئة والمطاعم والمقاهي',
 }
 
 export const mergeOpenGraph = (og?: Metadata['openGraph']): Metadata['openGraph'] => {
