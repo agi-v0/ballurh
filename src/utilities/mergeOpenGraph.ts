877b6b80f2414841fab3cf5ae29bda13cf3f127f
omar-963 <omar@byvalence.com>
2025-08-05 22:43:17 +0300

    add default og image

--- src/utilities/mergeOpenGraph.ts
+++ src/utilities/mergeOpenGraph.ts
@@ -5,11 +5,11 @@ const defaultOpenGraph: Metadata['openGraph'] = {
   type: 'website',
   description:
     '‏زد مبيعاتك، تنبأ بسلوكيات الطلب وقم بتحسين مؤشرات الأداء من خلال إتخاذ قرارات نمو تعتمد على البيانات باستخدام قوة تقنيات ذكاء الأعمال.',
-  // images: [
-  //   {
-  //     url: `${getServerSideURL()}/website-template-OG.webp`,
-  //   },
-  // ],
+  images: [
+    {
+      url: `${getServerSideURL()}/website-template-OG.webp`,
+    },
+  ],
   siteName: 'بلّورة',
   title: 'بلّورة | منصة ذكاء أعمال لقطاع التجزئة والمطاعم والمقاهي',
 }
