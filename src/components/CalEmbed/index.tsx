0c597c2dc98869227f14369bac757c2f5796bc59
omar-963 <omar@byvalence.com>
2025-08-06 09:50:05 +0300

    add UTM parameter support to CalEmbed component

--- src/components/CalEmbed/index.tsx
+++ src/components/CalEmbed/index.tsx
@@ -1,5 +1,6 @@
 'use client'
 import Cal from '@calcom/embed-react'
+import { useSearchParams } from 'next/navigation'
 
 interface CalEmbedProps {
   calLink: string
@@ -7,6 +8,14 @@ interface CalEmbedProps {
 }
 
 const CalEmbed: React.FC<CalEmbedProps> = ({ calLink, locale }) => {
+  const urlParams = useSearchParams()
+  const utm_id = urlParams.get('utm_id')
+  const utm_source = urlParams.get('utm_source')
+  const utm_medium = urlParams.get('utm_medium')
+  const utm_campaign = urlParams.get('utm_campaign')
+  const utm_content = urlParams.get('utm_content')
+  const utm_term = urlParams.get('utm_term')
+
   return (
     <Cal
       calLink={calLink}
@@ -18,6 +27,12 @@ const CalEmbed: React.FC<CalEmbedProps> = ({ calLink, locale }) => {
       config={{
         layout: 'month_view',
         theme: 'light',
+        utm_id: utm_id || '',
+        utm_source: utm_source || '',
+        utm_medium: utm_medium || '',
+        utm_campaign: utm_campaign || '',
+        utm_content: utm_content || '',
+        utm_term: utm_term || '',
         locale,
       }}
     />
