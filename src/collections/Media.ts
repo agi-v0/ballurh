7d940b590acb510eed3fe70353d5e4b2145f1437
omar-963 <omar@byvalence.com>
2025-08-14 07:27:17 +0300

    auto generate webp images

--- src/collections/Media.ts
+++ src/collections/Media.ts
@@ -87,6 +87,7 @@ export const Media: CollectionConfig = {
     staticDir: path.resolve(dirname, '../../public/media'),
     adminThumbnail: 'thumbnail',
     focalPoint: true,
+    formatOptions: { format: 'webp', options: { quality: 100 } },
     imageSizes: [
       {
         name: 'thumbnail',
